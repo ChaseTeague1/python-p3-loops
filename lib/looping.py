78e09507d3e0131cb7aa3b48d71c6a787e801cda
ChaseTeague <teaguechase@yahoo.com>
2024-02-19 07:22:23 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
+    i = 10
+    while i in range(10, 0, -1):
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
+    int_list = [integer ** 2 for integer in int_list]
+    return int_list
     pass
 
 def fizzbuzz():
     # code goes here!
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5: 
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
     pass
